731e05c560c22e352b723f27e78f083959480f65
bharteekhushee <bharteekhushee@gmail.com>
2026-06-22 22:12:05 +0530

    some changes

--- src/components/Home/HomeHero.jsx
+++ src/components/Home/HomeHero.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { homeHeroSlides } from "@/data/Home/homeHero";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import {
@@ -17,7 +15,6 @@ import "swiper/css/pagination";
 function HomeHero() {
   return (
     <section className="homeHero">
-
       <Swiper
         modules={[
           Pagination,
@@ -36,45 +33,29 @@ function HomeHero() {
           disableOnInteraction: false,
         }}
       >
-
         {homeHeroSlides.map((item) => (
-
           <SwiperSlide key={item.id}>
-
             <div className="homeHeroCard">
-
               <img
                 src={item.image}
                 alt={item.title}
                 className="homeHeroCard-image"
               />
-
               <div className="homeHeroCard-overlay"></div>
-
               <div className="container">
-
                 <div className="homeHeroCard-content">
-
                   <h1 className="heading light-heading homeHeroCard-title">
                     {item.title}
                   </h1>
-
                   <p className="description light-description">
                     {item.description}
                   </p>
-
                 </div>
-
               </div>
-
             </div>
-
           </SwiperSlide>
-
         ))}
-
       </Swiper>
-
     </section>
   );
 }
